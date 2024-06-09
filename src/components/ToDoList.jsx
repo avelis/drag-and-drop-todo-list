7ebeccd2988fede575fe65bec11f17f3825acdc7
Corina Udrescu <corinutz@gmail.com>
2024-06-09 11:34:44 +0200

    ability to add todos

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,11 @@
 import TaskColumn from "./TaskColumn";
 import { DndContext } from "@dnd-kit/core";
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 const ToDoList = ({ todos: initialTodos }) => {
   const [todos, setTodos] = useState(initialTodos);
+  const [newTodoText, setNewTodoText] = useState("");
 
   const handleDragEnd = ({ active, over }) => {
     const draggedTodoId = active.id;
@@ -29,8 +31,38 @@ const ToDoList = ({ todos: initialTodos }) => {
     );
   };
 
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    // add the todo
+    setTodos([
+      ...todos,
+      {
+        id: uuid,
+        text: newTodoText,
+        status: "to-do",
+      },
+    ]);
+    // clear the input
+    setNewTodoText("");
+  };
+
   return (
     <DndContext onDragEnd={handleDragEnd}>
+      <h2 style={{ marginLeft: "10px" }}>To do list</h2>
+      <form
+        onSubmit={handleAddTodo}
+        style={{ margin: "10px", display: "flex", gap: "10px" }}
+      >
+        <input
+          type="text"
+          name="newTodoText"
+          placeholder="type in your todo"
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
+        />
+        <button type="submit">Add todo</button>
+      </form>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <TaskColumn
           title="To do"
